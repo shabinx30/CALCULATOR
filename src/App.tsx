664d591427679e61a4fcaa0def8373c0c307783e
shabinx30 <shabeensharih@gmail.com>
2025-07-08 14:25:08 +0530

    refactor: update button structure and enhance input handling in calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { motion } from "framer-motion";
 import { buttons } from "./libs/buttons";
 
@@ -6,6 +6,7 @@ const App = () => {
     const containerRef = useRef<HTMLDivElement>(null);
     const [resultHeight, setResultHeight] = useState(200);
     const [input, setInput] = useState("");
+    const [fastResult, setFastResult] = useState("");
 
     const handleDrag = (_: unknown, info: { delta: { y: number } }) => {
         if (!containerRef.current) return;
@@ -17,14 +18,17 @@ const App = () => {
     };
 
     const handleInput = (value: string | boolean) => {
+        // checking if it's actually an operator or num
         if (value === false) {
             return setInput((p) => p.slice(0, p.length - 1));
         }
 
+        // checking if it's num
         value = String(value);
         if (!isNaN(parseInt(value))) {
-            return setInput((p) => {
+            setInput((p) => {
                 let nums = p.split(/[+×÷.-]/g);
+                // relaplacing 0 with num
                 if (parseInt(nums[nums.length - 1]) == 0) {
                     return p.slice(0, p.length - 1) + value;
                 }
@@ -32,21 +36,78 @@ const App = () => {
             });
         }
 
+        // checking if it's an operator
         if (value.match(/[+×÷.-]/g)) {
-            if (input[input.length - 1].match(/[+×÷.-]/g)) {
-                return setInput((p) => p.slice(0, input.length - 1) + value);
-            }
-            return setInput((p) => p + value);
+            // replacing operators with another
+            setInput((p) => {
+                const last = p[p.length - 1];
+                if (last && last.match(/[+×÷.-]/g)) {
+                    return p.slice(0, input.length - 1) + value;
+                }
+                return p + value;
+            });
         }
 
+        // clear the result section
         if (value === "AC") {
+            setFastResult("");
             return setInput("");
         }
+
+        // calculate the result
         if (value === "=") {
-            let exe = input.replaceAll("×", "*").replaceAll("÷", "/");
-            return setInput(String(new Function(`return ${exe}`)()));
+            calc();
         }
     };
+
+    const calc = () => {
+        setFastResult("");
+        setInput((p) => {
+            const exe = p.replaceAll("×", "*").replaceAll("÷", "/");
+            if (/^[0-9+\-*/().\s]+$/.test(exe)) {
+                return String(new Function(`return ${exe}`)());
+            } else {
+                return "Invalid input";
+            }
+        });
+    };
+
+    // for showing the quick results
+    useEffect(() => {
+        if (input) {
+            const nums = input.split(/[+×÷.-]/g);
+            if (nums.length > 1 && nums[nums.length - 1]) {
+                const exe = input.replaceAll("×", "*").replaceAll("÷", "/");
+                if (/^[0-9+\-*/().\s]+$/.test(exe)) {
+                    setFastResult(String(new Function(`return ${exe}`)()));
+                }
+            } else {
+                setFastResult("");
+            }
+        }
+    }, [input]);
+
+    useEffect(() => {
+        const handleKey = (e: KeyboardEvent) => {
+            const { key } = e;
+            if (key.toLowerCase() === "x" || key === "*") {
+                return handleInput("×");
+            } else if (key === "/") {
+                return handleInput("÷");
+            } else if (/^[0-9+\-*/().\s]+$/.test(key)) {
+                return handleInput(key);
+            } else if (key === "Backspace" || key === "Delete") {
+                return setInput((p) => p.slice(0, p.length - 1));
+            } else if (key === "Enter" || key === "=") {
+                return calc();
+            }
+        };
+
+        window.addEventListener("keydown", handleKey);
+        return () => {
+            window.removeEventListener("keydown", handleKey);
+        };
+    }, []);
 
     return (
         <div
@@ -59,9 +120,10 @@ const App = () => {
                     className="overflow-auto"
                     style={{ height: `${resultHeight}px` }}
                 >
-                    <div className="min-h-[10em] flex justify-end p-4">
-                        {/* Content goes here */}
-                        <p className="text-4xl font-semibold">{input}</p>
+                    <div className="min-h-[10em] flex flex-col items-end p-4">
+                        {/* result goes here */}
+                        <p className="text-5xl font-semibold">{input}</p>
+                        <p className="text-4xl text-gray-500">{fastResult}</p>
                     </div>
                 </section>
 
@@ -87,13 +149,13 @@ const App = () => {
                     <div className="flex gap-2 pb-2 h-[5em]">
                         <div
                             onClick={() => handleInput("AC")}
-                            className="flex-1/2 flex justify-center items-center c-btn text-[1.8em] rounded-full"
+                            className="flex-1/2 flex justify-center items-center bg-[#e0e0e0] text-black select-non text-[1.8em] rounded-full cursor-pointer"
                         >
                             AC
                         </div>
                         <div
                             onClick={() => handleInput("÷")}
-                            className="flex-1 flex justify-center items-center c-btn text-[2.2em] aspect-square rounded-full"
+                            className="flex-1 flex justify-center items-center bg-[#e0e0e0] text-black select-non text-[2.2em] aspect-square rounded-full cursor-pointer"
                         >
                             ÷
                         </div>
@@ -105,13 +167,20 @@ const App = () => {
                             <div
                                 onClick={() =>
                                     handleInput(
-                                        typeof item === "string" && item
+                                        typeof item.icon === "string" &&
+                                            item.icon
                                     )
                                 }
                                 key={i}
-                                className="flex justify-center items-center rounded-full c-btn text-[1.8em]"
+                                className={`${
+                                    item.isOper
+                                        ? "flex justify-center items-center rounded-full cursor-pointer bg-[#e0e0e0] text-black select-none text-[1.8em]"
+                                        : item.isDelete
+                                        ? "flex justify-center items-center rounded-full cursor-pointer bg-red-400 text-black select-none text-[1.8em]"
+                                        : "flex justify-center items-center rounded-full cursor-pointer bg-[#1b1b1b] select-none text-[1.8em]"
+                                }`}
                             >
-                                {item}
+                                {item.icon}
                             </div>
                         ))}
                     </div>
